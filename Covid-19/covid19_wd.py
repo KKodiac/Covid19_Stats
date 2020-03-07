973abad90c93f28f5c14334daaf496bd82afbda0
Sean Hong <seanhong2000@gmail.com>
2020-03-07 14:57:47 +0000

    Initial Commit

--- Covid-19/covid19_wd.py
+++ Covid-19/covid19_wd.py
@@ -24,7 +24,7 @@ Query Note:
     
 """
 
-
+import csv, json, sys
 from os import path
 import json 
 import requests
@@ -77,4 +77,19 @@ class CovidInfowd:
         print(att)
         
         
-        
+    def jsontocsv(self):
+            
+        #if you are not using utf-8 files, remove the next line
+        sys.setdefaultencoding("UTF-8") #set the encode to utf8
+        #check if you pass the input file and output file
+        if sys.argv[1] is not None and sys.argv[2] is not None:
+            fileInput = sys.argv[1]
+            fileOutput = sys.argv[2]
+            inputFile = open(fileInput) #open json file
+            outputFile = open(fileOutput, 'w') #load csv file
+            data = json.load(inputFile) #load json content
+            inputFile.close() #close the input file
+            output = csv.writer(outputFile) #create a csv.write
+            output.writerow(data[0].keys())  # header row
+            for row in data:
+                output.writerow(row.values()) #values row
