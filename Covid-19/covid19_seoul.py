1791e76e16f83fe26a718fe51de2def19583b516
noopy <young_ahn@yonsei.ac.kr>
2020-03-14 21:49:02 +0900

    seoul confirmed data collected as function based

--- Covid-19/covid19_seoul.py
+++ Covid-19/covid19_seoul.py
@@ -25,7 +25,7 @@ soup = BeautifulSoup(res.content, "lxml")
 
 # use table html tag to find tables in the webpage:
 tables = soup.find_all("table")
-print(len(tables))
+# print(len(tables))
 
 # crawl all table data from the webpage: this includes unwanted data
 for i in range(0, len(tables)):
@@ -38,6 +38,8 @@ for i in range(0, len(tables)):
         + ".csv"
     )
 
+
+df_confirmed = pd.DataFrame(dummy_data1)
 # validate all collected csv data from seoul
 for i in range(0, len(tables)):
     # read all csv files in folder
@@ -46,8 +48,15 @@ for i in range(0, len(tables)):
         + str(i)
         + ".csv"
     )
-    print(len(df_not_validated.index))
 
+    if len(df_not_validated.index) < 10:
+        pass
+    else:
+        columns_list = df_not_validated.columns
+        if "환자" in columns_list:
+            # print(df_not_validated.head())
+            df_confirmed = pd.concat([df_confirmed, df_not_validated])
+df_confirmed.to_csv("/Covid-19/Data/Korea/covid_dat_seoul.csv")
 
 """
 class CovidInfoSeoul:
