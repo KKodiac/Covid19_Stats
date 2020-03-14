3991b5630dc40e9da20c7e4dbe2ae09892ff628d
noopy <young_ahn@yonsei.ac.kr>
2020-03-14 21:07:32 +0900

    add crawler for seoul timeseries data

---
+++ Covid-19/covid19_seoul.py
@@ -0,0 +1,31 @@
+# This one gets all table data text
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import csv
+from urllib.parse import urlparse
+
+# access to seoul corona data panel
+url = "http://www.seoul.go.kr/coronaV/coronaStatus.do"
+
+# create dummy dataframe
+dummy_data1 = {}
+df = pd.DataFrame(dummy_data1)
+
+# get page content response from the web using requests and beautifulsoup
+res = requests.get(url)
+soup = BeautifulSoup(res.content, "lxml")
+
+# find table tag
+table = soup.find_all("table")[1]
+print(len(table))
+
+# using panda's read_html to crawl tabledata
+df_crawl = pd.read_html(str(table), encoding="utf-8", header=0)[0]
+
+# save as csv file
+df_crawl.to_csv(
+    "/Users/noopy/_forks/Covid19_Stats/Covid-19/Data/Korea/seoul_timeseries.csv"
+)
