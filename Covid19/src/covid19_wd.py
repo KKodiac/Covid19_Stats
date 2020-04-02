283cd79a252a8fe031fe5bf6891a33cc5c055bee
Sean Hong <seanhong2000@gmail.com>
2020-04-02 13:51:43 +0000

    Today's update on Covid19

---
+++ Covid19/src/covid19_wd.py
@@ -0,0 +1,52 @@
+import requests 
+import csv
+from os import path 
+from bs4 import BeautifulSoup as bs4
+from time import ctime, time
+from shutil import copyfile
+
+class CovidWorldInfo:
+    def __init__(self):
+        self.today = ctime(time())
+        self.scrape_url = "https://www.worldometers.info/coronavirus/"
+        self.datafile = f"./Covid19/Data/World/new_covid_dat.csv"
+        self.appendfile = f"./Covid19/Data/World/world_timeseries/{self.today}.csv"
+        
+    def getData(self):
+        page = requests.get(self.scrape_url)
+        html = bs4(page.text, 'html.parser')
+        table = html.find(id="main_table_countries_today")
+        
+        thead_all = table.thead.find_all('th')
+        
+        thead = [th.text for th in thead_all]
+        
+        tbody_all = table.find_all('tbody')
+        tr_temp = [tr for tr in tbody_all[0].find_all('tr')]
+        td_temp = [td.find_all('td') for td in tr_temp]
+        tbody = [[j.text.strip() for j in i] for i in td_temp]
+        
+        return thead, tbody
+        
+    
+    
+    def inputData(self):
+        thead, tbody = self.getData()
+    
+        with open(self.datafile, 'w+') as file:
+            cw = csv.writer(file)
+            cw.writerow(thead)
+            for i in tbody:
+                cw.writerow(i)
+            file.close()
+            
+    # this function should run before inputData()
+    def worldTimeseries(self):
+        with open(self.datafile) as file:
+            if(path.isfile(self.datafile)==True):
+                appendfile = open(self.appendfile, 'w+')
+                copyfile(self.datafile, self.appendfile)
+    
+    def run(self):
+        self.worldTimeseries()
+        self.inputData()
---
+++ main.py
@@ -0,0 +1,14 @@
+from Covid19.src.covid19_kr import CovidInfokr
+from Covid19.src.covid19_wd import CovidWorldInfo
+from Covid19.src.covid19_seoul import CovidInfoSeoul
+
+if __name__ == '__main__':
+    covidkr = CovidInfokr() # Korean Stats for Covid-19
+    covidkr.run()
+    
+    covidwd = CovidWorldInfo() # new World Stats that has more accurate
+    covidwd.run()
+    
+    
+    seoul = CovidInfoSeoul()
+    seoul.run()
