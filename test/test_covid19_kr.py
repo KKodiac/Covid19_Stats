118ef3722ed04242ed69b62aac8d821d33264a80
Sean Hong(홍성민) <seanhong2000@gmail.com>
2021-05-26 01:21:30 +0900

    Update test

--- test/test_covid19_kr.py
+++ test/test_covid19_kr.py
@@ -1,11 +1,11 @@
 import os,sys
 os.path.abspath(os.pardir)
-from Covid19.src.covid19_kr import CovidInfokr
 import unittest
+import pytest
 
 class TestCovid19Kr(unittest.TestCase):
     def setUp(self):
         self.app = CovidInfokr()
 
     def test_covid_kr_run(self):
-        assert(self.app, -1)
+        pass
